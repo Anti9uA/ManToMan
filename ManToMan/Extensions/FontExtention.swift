81d090f377918aef5ed812791a940cf0f3fb0530
Anti9uA <didlaak6000@naver.com>
2023-02-05 03:19:16 +0900

    [feat] extension 틀 만들어놓기

---
+++ ManToMan/Extensions/FontExtention.swift
@@ -0,0 +1,8 @@
+//
+//  FontExtention.swift
+//  ManToMan
+//
+//  Created by Kyubo Shim on 2023/02/05.
+//
+
+import Foundation
---
+++ ManToMan/Extensions/TextExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TextExtension.swift
+//  ManToMan
+//
+//  Created by Kyubo Shim on 2023/02/05.
+//
+
+import Foundation
