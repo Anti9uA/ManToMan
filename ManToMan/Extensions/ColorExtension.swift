1a5ee92aa63ddb090a4bc574eaa40c0540b65f0a
Eunbi Cho <moomb70@gmail.com>
2023-02-05 18:54:48 +0900

    컬러 익스텐션 추가

---
+++ ManToMan/Extensions/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  ColorExtension.swift
+//  ManToMan
+//
+//  Created by 조은비 on 2023/02/05.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    // 피그마에 등록된 색상 보시고, 사용하시면 됩니다!
+    // foregroundColor(.background) 이런식으로 사용하시면 돼요!
+    
+    //custom 색상
+    static let background = Color("background")
+    static let customDarkGray = Color("darkGray")
+    static let customLightGray = Color("lightGray")
+    
+    //main color
+    static let mainBlue = Color("mainBlue")
+    static let mainRed = Color("mainRed")
+
+    //disabled color
+    static let disabledBlack = Color("disabledBlack")
+    static let disabledBlue = Color("disabledBlue")
+
+}
--- ManToMan/Extensions/FontExtension.swift
+++ ManToMan/Extensions/FontExtension.swift
@@ -11,7 +11,7 @@ import SwiftUI
 extension Font {
     // 피그마에 사용된 폰트 보시고, 사용하시면 됩니다!
     // font(Font.customTitle()) 이런식으로 사용하시면 돼요!
-         
+    
     static func customTitle() -> Font {
         return Font.custom("LINESeedSansKR-Bold", size: 20)
     }
@@ -24,6 +24,7 @@ extension Font {
         return Font.custom("LINESeedSansKR-Bold", size: 14)
     }
     
+    // 번역체 모음
     static func korean() -> Font {
         return Font.custom("LINESeedSansKR-Bold", size: 24)
     }
